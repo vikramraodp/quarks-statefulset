c6b16943be8b18a5e43279ef1cf142649c3565fe
Mario Manno <mario.manno@suse.com>
2020-07-09 13:31:31 +0200

    Remove quarks-operator files

--- cmd/cf-operator/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import cmd "code.cloudfoundry.org/quarks-operator/cmd/internal"
-
-func main() {
-	cmd.Execute()
-}
---
+++ cmd/quarks-statefulset/main.go
@@ -0,0 +1,7 @@
+package main
+
+import cmd "code.cloudfoundry.org/quarks-statefulset/cmd/internal"
+
+func main() {
+	cmd.Execute()
+}
