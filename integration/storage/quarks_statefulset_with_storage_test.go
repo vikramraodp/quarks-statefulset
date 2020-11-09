f91f00f8b42e611e8374fd79b162e5e69d63f65c
Ettore Di Giacinto <edigiacinto@suse.com>
2020-11-09 16:40:54 +0100

    Add test to trigger current bug

--- integration/storage/quarks_statefulset_with_storage_test.go
+++ integration/storage/quarks_statefulset_with_storage_test.go
@@ -6,6 +6,7 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 
 	qstsv1a1 "code.cloudfoundry.org/quarks-statefulset/pkg/kube/apis/quarksstatefulset/v1alpha1"
 	"code.cloudfoundry.org/quarks-utils/testing/machine"
@@ -77,6 +78,56 @@ var _ = Describe("QuarksStatefulSet", func() {
 			out, err := env.GetPodLogs(env.Namespace, podName)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(string(out)).To(Equal("present\n"))
+		})
+
+		FIt("should prune volume templates when qsts gets updated accordingly", func() {
+			By("Adding volume write command to pod spec template")
+			quarksStatefulSet.Spec.Template.Spec.Template.Spec.Containers[0].Image = "opensuse/leap:15.1"
+			quarksStatefulSet.Spec.Template.Spec.Template.Spec.Containers[0].Command = []string{"/bin/bash", "-c", "echo present > /etc/random/presentFile ; sleep 3600"}
+
+			By("Creating an QuarksStatefulSet")
+			ess, tearDown, err := env.CreateQuarksStatefulSet(env.Namespace, quarksStatefulSet)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(ess).NotTo(Equal(nil))
+			defer func(tdf machine.TearDownFunc) { Expect(tdf()).To(Succeed()) }(tearDown)
+
+			By("Checking for pod")
+			err = env.WaitForPods(env.Namespace, "testpod=yes")
+			Expect(err).NotTo(HaveOccurred())
+
+			ess, err = env.GetQuarksStatefulSet(env.Namespace, ess.GetName())
+			Expect(err).NotTo(HaveOccurred())
+			Expect(ess).NotTo(Equal(nil))
+			ess.Spec.Template.Spec.Template.ObjectMeta.Labels["testpodupdated"] = "yes"
+			vols := []corev1.Volume{{
+				Name: "pvc",
+				VolumeSource: corev1.VolumeSource{
+					EmptyDir: &corev1.EmptyDirVolumeSource{},
+				},
+			}}
+			ess.Spec.Template.Spec.Template.Spec.Volumes = vols
+			ess.Spec.Template.Spec.VolumeClaimTemplates = nil
+			By("Updating the QuarksStatefulSet")
+
+			essUpdated, tearDown, err := env.UpdateQuarksStatefulSet(env.Namespace, *ess)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(essUpdated).NotTo(Equal(nil))
+			defer func(tdf machine.TearDownFunc) { Expect(tdf()).To(Succeed()) }(tearDown)
+			By("Checking for sts")
+			err = env.WaitForPods(env.Namespace, "testpodupdated=yes")
+			Expect(err).NotTo(HaveOccurred())
+
+			ess, err = env.GetQuarksStatefulSet(env.Namespace, ess.GetName())
+			Expect(err).NotTo(HaveOccurred())
+			Expect(ess).NotTo(Equal(nil))
+
+			Expect(ess.Spec.Template.Spec.VolumeClaimTemplates).To(BeNil())
+			Expect(ess.Spec.Template.Spec.Template.Spec.Volumes).To(Equal(vols))
+
+			ss, err := env.GetStatefulSet(env.Namespace, ess.GetName())
+			Expect(err).NotTo(HaveOccurred())
+			Expect(ss.Spec.VolumeClaimTemplates).To(BeNil())
+			Expect(ss.Spec.Template.Spec.Volumes).To(Equal(vols))
 		})
 	})
 })
